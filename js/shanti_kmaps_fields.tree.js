0d9ccb90f8e7cc2646c9f35440dc5fb832529652
Raf Alvarado <ontoligent@gmail.com>
2015-10-08 09:44:55 -0400

    Hacks

--- js/shanti_kmaps_fields.tree.js
+++ js/shanti_kmaps_fields.tree.js
@@ -14,7 +14,7 @@ Drupal.behaviors.shantiKmapsFieldsTree = {
   
     // Grab settings from server
     S = settings.shanti_kmaps_fields;
-    console.log('t');
+    console.log('field_id: ' + S.field_id);
 		    
     // Event handler 0: On first load, go through each instance of the field and update its picklist
     $('#'+S.field_id).once(function(){
