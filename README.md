f199a54ba4319819ee0af8edecda2f5e3b90f005
Edward Garrett <edward@pinedrop.com>
2015-10-15 22:28:10 +0100

    initial commit of kmaps_typeahead_picker widget

--- README.md
+++ README.md
@@ -12,12 +12,17 @@ A field for adding KMap IDs from the KMaps API to Drupal assets and updating the
 KMaps Places Explorer. The current default value is "http://badger.drupal-dev.shanti.virginia.edu/subjects/__KMAPID__", where \_\_KMAPID\_\_ is a placeholder for the actual KMap ID when called.
   4. *KMaps Places Explorer*: This is the site used to view and explore KMap place terms in the SHANTI KMap Solr Index. The current default value is "http://badger.drupal-dev.shanti.virginia.edu/places/__KMAPID__", where \_\_KMAPID\_\_ is a placeholder for the actual KMap ID when called.
   5. *Do you want to publish your KMap data to the SHANTI KMap Solr Index?* Check this if you want to publish your content for use by other sites, such as the Subjects and Places explorers described above. 
-  6. *KMaps Solr Server*. The URL to the SHANTI KMap index. Note that this URL may need to include a path to the specific index. The current default value is "http://kidx.shanti.virginia.edu/solr/kmindex".
+  6. *KMaps Solr Server Assets Index*. The URL to the SHANTI KMap Assets index. Note that this URL may need to include a path to the specific index. The current default value is "http://kidx.shanti.virginia.edu/solr/kmindex".
+  7. *KMaps Solr Server Terms Index*. The URL to the SHANTI KMap Terms index. Note that this URL may need to include a path to the specific index. The current default value is "http://kidx.shanti.virginia.edu/solr/termindex-dev".
+  8. *KMaps Solr Asset Types*. A comma delimited list of asset types, used throughout the KMap system. These values should be lowercase plurals with no spaces. Ideally, this list would come from somewhere authoritative, such as the Rail KMaps server itself.
 7. Add a field of type **KMap Term** to a content type.
-  1. Choose a widget type.
-  2. Under Field Settings, choose the KMap domain -- subjects or places -- to be associatd with this field wherever it is used.
-  3. Don't worry about "Number of vaules" under Field Settings if you chose the **Tree** widget type.
+  1. Choose a widget type, **Tree** or **Autocomplete**.
+  2. Under Field Settings, choose the KMap domain -- subjects or places -- to be associated with this field wherever it is used.
+  3. Don't worry about "Number of values" under Field Settings if you chose the **Tree** or **Autocomplete** widget type.
   3. Under the Field instance settings (under the "Edit" tab), optionally add a "KMap ID View." This is a local Drupal path for a view that needs to be created with which to search for nodes of this content type by KMap ID. This value is used by the popover field formatter. Use \_\_KMAPID\_\_ to signify the KMap ID value in the path.
+  3. Also under the Field instance settings, optionally add a "Root KMap ID". This restricts terms to occur within a particular hierarchy.
+  2. Also under the Field instance settings, set an optional KMap Term Limit, which specifies the maximum number of matched terms to return from a KMap search. This is important for the **Autocomplete** widget, because higher limits may lead to slower search. Enter 0 for no limit.
 8. In choose a field formatter, under Manage Display, don't choose **popover with link options** unless you are using a SHANTI Sarvaka theme. 
 9. Create a node of the content type associated with the field type. 
 10. If you are using the **Tree** widget, enter a search string into the text field and click on the "Search" button. If you see a KMap term you want associated with your node, click on it and it will appear in the list of associated terms below the tree. You can add as many terms as you'd like. You may delete terms from the list by clicking on the "X". 
+11. If you are using the **Autcomplete** widget, start typing a search string into the text field. Select a term from the list that pops up, or type something else into the field.
---
+++ js/shanti_kmaps_fields.typeahead.js
@@ -0,0 +1,96 @@
+/**
+ * Created by edwardjgarrett on 10/14/15.
+ */
+
+(function ($) {
+    Drupal.behaviors.kmaps_typeahead = {
+        attach: function (context, settings) {
+            $('.kmaps-typeahead', context).once('kmaps-fields').each(function () {
+                var admin_settings = settings.shanti_kmaps_admin;
+                var widget_settings = settings.shanti_kmaps_fields[$(this).attr('id').replace('_search_term','')];
+                var index = admin_settings.shanti_kmaps_admin_server_solr_terms;
+                var domain = widget_settings.domain;
+                var limit = widget_settings.term_limit == 0 ? 999 : widget_settings.term_limit;
+                var separator = ' - ';
+                var field = 'name';
+                var preq = '&q=' + field + ':';
+                var filters = [];
+                if (admin_settings.shanti_kmaps_admin_solr_filter_query) {
+                    filters.push(admin_settings.shanti_kmaps_admin_solr_filter_query);
+                }
+                if (widget_settings.root_kmapid) {
+                    filters.push('ancestor_ids_default:' + widget_settings.root_kmapid);
+                }
+                var params = {
+                    'wt': 'json',
+                    'indent': true,
+                    'fq': filters.concat(['tree:' + domain]),
+                    'fl': 'id,ancestors',
+                    'rows': limit,
+                    'hl': true,
+                    'hl.fl': field,
+                    'hl.simple.pre': '',
+                    'hl.simple.post': ''
+                };
+                var url = index + '/select?' + $.param(params, true);
+                var terms = new Bloodhound({
+                    datumTokenizer: Bloodhound.tokenizers.obj.whitespace('value'),
+                    queryTokenizer: Bloodhound.tokenizers.whitespace,
+                    remote: {
+                        url: url,
+                        replace: function () {
+                            var q = url;
+                            var val = $('.kmaps-tt-input').val();
+                            if (val) {
+                                q += preq + encodeURIComponent(val.replace(/\s/g, '\\ ') + '*');
+                            }
+                            return q;
+                        },
+                        filter: function (json) {
+                            return $.map(json.response.docs, function (doc) {
+                                var ancestors = doc.ancestors;
+                                ancestors.reverse().shift();
+                                return {
+                                    id: doc.id,
+                                    ancestors: ancestors.join(separator),
+                                    value: json.highlighting[doc.id][field][0] //take first highlight
+                                };
+                            });
+                        }
+                    }
+                });
+                terms.initialize();
+                $('.kmaps-typeahead').typeahead(
+                    {
+                        highlight: false,
+                        hint: true,
+                        classNames: {
+                            input: 'kmaps-tt-input',
+                            hint: 'kmaps-tt-hint',
+                            menu: 'kmaps-tt-menu',
+                            dataset: 'kmaps-tt-dataset',
+                            suggestion: 'kmaps-tt-suggestion',
+                            empty: 'kmaps-tt-empty',
+                            open: 'kmaps-tt-open',
+                            cursor: 'kmaps-tt-cursor',
+                            highlight: 'kmaps-tt-highlight'
+                        }
+                    },
+                    {
+                        name: domain,
+                        limit: 999,
+                        display: 'value',
+                        source: terms,
+                        templates: {
+                            suggestion: function (data) {
+                                return '<div><span class="kmaps-term">' + data.value + '</span> ' +
+                                    '<span class="kmaps-ancestors">' + data.ancestors + '</span></div>';
+                            }
+                        }
+                    }
+                );
+            });
+        }
+    }
+})
+(jQuery);
