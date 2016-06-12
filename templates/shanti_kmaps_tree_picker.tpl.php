ee6b42376ee4ec60931a67998f885930d61ee811
Edward Garrett <edward@pinedrop.com>
2016-06-12 16:56:25 +0100

    position selected kmap terms after tree

---
+++ templates/shanti_kmaps_tree_picker.tpl.php
@@ -0,0 +1,14 @@
+<!--
+ Available variables:
+   $namespace - used by the javascript to coordinate related filters and typeahead inputs
+   $display - the user-facing unpluralized filter name, Feature Type or Associated Subject
+-->
+
+<div id="<?php print $namespace; ?>-tree-wrapper"
+     class="kmap-tree-picker">
+    <label><span>Search:</span> Select KMaps <?php print $domain; ?></label>
+    <input id="<?php print $namespace; ?>-search-term"
+           class="kmap-search-term form-control form-text" type="text"
+           placeholder="Search <?php print $domain; ?>">
+    <button type='button' id='<?php print $namespace; ?>-search-button' name='<?php print $namespace; ?>-search-button' class='kmap-search-button' value=''>SEARCH</button>
+</div>
